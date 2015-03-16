5643b2d4504c549af488294f06dada53568de382
Chris Pendleton <chrispendleton@mycwi.cc>
2015-03-16 11:29:57 -0600

    Initial commit.

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Sandbox\WEBD265\Project 5\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Sandbox\WEBD265\Project 5\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Sandbox\WEBD265\Project 5\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Sandbox\WEBD265\Project 5\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Sandbox\WEBD265\Project 5\platforms\android\ant-build\classes.dex \
+C:\Sandbox\WEBD265\Project 5\platforms\android\CordovaLib\ant-build\classes.jar \
